f3d9f702f20a12ae6f74097dc77d1ae4a7fed05b
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-07-18 19:53:45 -0300

    Feat: Created tests

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
